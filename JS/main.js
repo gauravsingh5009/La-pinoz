98b1da9ffe78cad78980373146c05a5355b04009
gauravsingh5009 <2000gauravjnp@gmail.com>
2024-12-04 19:08:04 +0530

    update checkout

--- JS/main.js
+++ JS/main.js
@@ -319,5 +319,67 @@ $(document).ready(function () {
   });
 
 
+  // cart
+  $('.cart-button .btn').on('click', function() {
+    // Hide the empty cart message
+    $('#empty-cart').addClass('d-none');
 
+    // Show the cart item total section
+    $('#add-cart').removeClass('d-none');
+  });
+
+
+  
+  // checkout
+  $('#openModal').click(function() {
+    $('#myModal').fadeIn();  
+  });
+  // Close the button
+  $('#closeModal').click(function() {
+    $('#myModal').fadeOut();  
+  });
+
+  // Open New Address 
+  $('#addNewAddress').click(function() {
+    $('#myModal').fadeOut();
+    $('#newAddress').fadeIn();  
+  });
+
+  // BackButton
+  $('#backBtn').click(function() {
+    $('#newAddress').fadeOut();
+  });
+
+  // Search Location Manually
+  $('#selectManualArea').click(function() {
+    $('#newAddress').fadeOut();
+    $('#SearchLocation').fadeIn();
+  });
+
+  // Close Secrch Location
+  $('#closeSearchLocation').click(function() {
+    $('#SearchLocation').fadeOut();
+  });
+
+  // Detail Address
+  $('.selectLocalities').click(function() {
+    $('#SearchLocation').fadeOut();
+    $('#fullAddress').fadeIn();
+  });
+
+  // Close Detail Button
+  $('#closeDetailsBtn').click(function() {
+    $('#fullAddress').fadeOut();
+  });
+
+  // Apply Coupon
+  $('#moreOffers').click(function() {
+    $('#coupon').fadeIn();
+  });
+  // close button
+  $('#circleBackBtn').click(function() {
+    $('#coupon').fadeOut();
+  });
+
+  
 });
